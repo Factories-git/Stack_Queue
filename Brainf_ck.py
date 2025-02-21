40f34966d7e5fa5adbe525fe4a5d98b00d45bbb4
Factories-git <official.factories@gmail.com>
2025-02-21 14:36:11 +0900

    Changes

--- Brainf_ck.py
+++ Brainf_ck.py
@@ -1,17 +1,51 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-def myWhile(ptr):
-    global memory
 
+def my_while(ptr, stack, idx):
+    s_idx = idx
+    global memory, s
+    stack.append(idx)
+    while True:
+        if idx == len(ipts):
+            if stack:
+                return False
+        j = ipts[idx]
+
+        if j == '>':
+            ptr = (ptr + 1) % 32768
+        elif j == '<':
+            ptr = (ptr - 1) % 32768
+        elif j == '+':
+            memory[ptr] = (memory[ptr] + 1) % 255
+        elif j == '-':
+            memory[ptr] = (memory[ptr] - 1) % 255
+        elif j == '.':
+            s.append(chr(memory[ptr]))
+        elif j == '[':
+            if s_idx != idx:
+                my_while(ptr, stack, idx)
+            else:
+                return False
+        elif j == ']':
+            if memory[ptr] == 1:
+                stack.pop()
+                return True
+            else:
+                idx = stack[-1]
+                continue
+        idx += 1
 
 
 for _ in range(1, int(input()) + 1):
     pointer = 0
     ipts = ''
     memory = [0] * 32768
-    repeat = []
+    s = []
+    flag = False
+    re = []
     while True:
         ipt = input().rstrip()
         if ipt == 'end':
@@ -29,10 +63,22 @@ for _ in range(1, int(input()) + 1):
         elif j == '-':
             memory[pointer] = (memory[pointer] - 1) % 255
         elif j == '.':
-            print(memory[pointer])
-            print(chr(memory[pointer]), end='', )
+            s.append(chr(memory[pointer]))
         elif j == '[':
-            repeat.append(idx)
+            if idx == len(ipts):
+                print('COMPILE ERROR')
+                flag = True
+                break
+            k = my_while(pointer, [], idx + 1)
+            re.append(idx + 1)
+            if not k:
+                print('COMPILE ERROR')
+                flag = True
+                break
+            continue
+        elif j == ']':
+            if not re:
+                print('COMPILE ERROR')
+                flag = True
 
-
-    print()
+    print(''.join(s) if not flag else '')
